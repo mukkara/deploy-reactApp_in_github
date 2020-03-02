957671ca32e3a55447a3f805bd450ceab1ced779
mukkara <rsr.mukkara452gmail.com>
2020-03-02 18:18:31 +0530

    Updates

---
+++ precache-manifest.69ec08d2ee765c41758e5d241dbe426d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e39ca8b90c4d6fcc0cd5f220cb02e4",
+    "url": "/deploy-reactApp_in_github/index.html"
+  },
+  {
+    "revision": "ed70fd30511b23411088",
+    "url": "/deploy-reactApp_in_github/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "08194936469b69e63ee4",
+    "url": "/deploy-reactApp_in_github/static/js/2.bc5b117a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy-reactApp_in_github/static/js/2.bc5b117a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed70fd30511b23411088",
+    "url": "/deploy-reactApp_in_github/static/js/main.72dd3bc2.chunk.js"
+  },
+  {
+    "revision": "d4b9e96d4a2e9caf32e4",
+    "url": "/deploy-reactApp_in_github/static/js/runtime-main.0d133000.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-reactApp_in_github/static/media/logo.5d5d9eef.svg"
+  }
+]);
